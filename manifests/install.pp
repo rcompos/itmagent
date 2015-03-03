e931207ad2eb7e063169973ded015fe94e534b63
Ron Compos <rcompos@gmail.com>
2015-03-02 22:43:07 -0500

    Add prereq installer.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,31 +1,5 @@
 class itmagent::install inherits itmagent {
 
-#  $application_url =
-#    "${maven_repo}/${_group_id}/${artifactid}/${version}/${artifactid}-${version}.${packaging}"
-#
-#  ::wget::fetch { $application_url:
-#    destination => "${catalina_base}/webapps/${war_name}", 
-#    cache_dir   => '/var/cache/wget',
-#    cache_file  => "${artifactid}-${version}.${packaging}",
-#    execuser   => 'tomcat',
-#  }
-
-#   file { 'test.txt':
-#      path  => '/tmp/test.txt',
-#      mode  => '0644',
-#      source => "puppet:///modules/itmagent/test.txt",
-#   }
-
-#   exec { "touch ${dir_dest}/${itm_file}":
-#      cwd     => "${dir_dest}",
-#      path    => '/usr/local/bin/:/bin/',
-#   }
-
-#   include wget
-#   wget::fetch { "$url_src":
-#      destination => "$dir_dest/$itm_file", 
-#   }
-#
    exec { "cp -a $mount_dir/$script_file .":
       cwd     => "$dir_dest",
       path    => "/usr/local/bin/:/bin/",
@@ -36,9 +10,15 @@ class itmagent::install inherits itmagent {
       path    => "/usr/local/bin/:/bin/",
    }
 
-   exec { "./itm630agent_rhel.sh $itm_server":
+   exec { "prereq_inst.sh":
       cwd     => "$dir_dest/$script_dir",
-      path    => ".:/usr/local/bin/:/bin/",
+      path    => "$dir_dest/$script_dir/:/usr/local/bin/:/bin/",
+   }
+
+   exec { "itm630agent_rhel.sh $itm_server":
+      cwd     => "$dir_dest/$script_dir",
+      #path    => ".:/usr/local/bin/:/bin/",
+      path    => "$dir_dest/$script_dir/:/usr/local/bin/:/bin/",
       # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
 #      unless  => " $itm_home/bin/cinfo -t lz"
    }
