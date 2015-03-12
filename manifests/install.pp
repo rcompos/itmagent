fbfa2953e53caa7fe873d2de7eef4cf774d6b09f
Ron Compos <rcompos@gmail.com>
2015-03-12 12:46:55 -0400

    Runs clean.

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,53 +28,49 @@ class itmagent::install (
 
    exec { "service rpcbind start":
       cwd     => "$dir_tmp",
-      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_tmp/$script_dir",
+      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:",
       unless  => "service rpcbind status",
-      creates => "$itm_home/bin/cinfo",
       logoutput => "true",
    }
 
    exec { "mkdir -p $mnt_dir":
       cwd     => "$dir_tmp",
-      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_tmp/$script_dir",
-      unless  => "ls $mnt_dir",
-      creates => "$itm_home/bin/cinfo",
+      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:",
+      creates => "$mnt_dir",
       logoutput => "true",
    }
    exec { "mount $nfs_options ${nfs_host}:${nfs_dir} $mnt_dir":
       cwd     => "$dir_tmp",
-      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_tmp/$script_dir",
+      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:",
       unless  => "grep ${nfs_dir} /etc/mtab",
-      creates => "$itm_home/bin/cinfo",
+      creates => "$dir_tmp/$script_file",
       logoutput => "true",
    }
 
    exec { "cp -a $mnt_dir/$src_dir/$script_file .":
       cwd     => "$dir_tmp",
       path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
-      #unless  => "ls $itm_home/bin/cinfo",
-      creates => "$itm_home/bin/cinfo",
+      creates => "$dir_tmp/$script_file",
       logoutput => "true",
    }
    exec { "tar xzf $script_file":
       cwd     => "$dir_tmp",
       path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
       #unless  => "ls $itm_home/bin/cinfo",
-      creates => "$itm_home/bin/cinfo",
+      creates => "$dir_tmp/$script_dir",
       logoutput => "true",
    }
 
    exec { "itm630agent_rhel.sh $itm_server $mnt_dir/$src_dir/$itm_dir":
       cwd     => "$dir_tmp/$script_dir",
       path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_tmp/$script_dir",
-      #unless  => "ls $itm_home/bin/cinfo",
       creates => "$itm_home/bin/cinfo",
       logoutput => "true",
    }
 
    exec { "umount ${nfs_host}:${nfs_dir}":
       cwd     => "$dir_tmp",
-      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$dir_tmp/$script_dir",
+      path => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:",
       onlyif => "grep ${nfs_dir} /etc/mtab",
       unless => "grep ${nfs_dir} /etc/fstab",
       logoutput => "true",
